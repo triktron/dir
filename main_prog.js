d14d9bb77405f057e89011fa85c869135899e3f3
triktron <ele.trik@hotmail.com>
2013-12-14 22:43:15 +0100

    2

---
+++ main_prog.js
@@ -0,0 +1,11 @@
+var s_w=window.innerWidth
+
+var s_h=window.innerHeight
+
+//--------------- id's -----------------------------------
+var text_ar_1 = document.getElementById("text_ar_1");
+
+//--------------- styling --------------------------------
+text_ar_1.style.position = "absolute";
+text_ar_1.style.height = ((s_h - 20)*2) + "px";
+text_ar_1.style.top = 20 + "px";
