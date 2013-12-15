724a8a232a818980196180610c949eee879dff5d
triktron <ele.trik@hotmail.com>
2013-12-15 17:48:54 +0100

    5

--- main_prog.js
+++ main_prog.js
@@ -7,6 +7,8 @@ var text_ar_1 = document.getElementById("text_ar_1");
 var text_sh_1 = document.getElementById("text_sh_1");
 
 //--------------- var's --------------------------------
+var video_chat;
+var chat;
 var temp_text = "";
 var items = [["hallo","#0000CD"],["world","#008000"],["raf","Transparent"],["emma","#F06"]];
 
@@ -21,5 +23,15 @@ function update_show() {
 	setTimeout(function(){update_show()},1000/30);
 }
 
+function on_close() {
+	chat.close();
+}
+
 //--------------- startup ---------------------------------
 update_show();
+video_chat = confirm("want to join chat");
+if (video_chat==true) {
+	chat = window.open("","MsgWindow","width=200,height=200");
+	chat.document.write("hallo");
+	chat.open();
+}
