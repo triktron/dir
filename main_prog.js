77b0c75be235ce4c231833461e6f75ce1209e0b0
triktron <ele.trik@hotmail.com>
2013-12-15 12:51:39 +0100

    3

--- main_prog.js
+++ main_prog.js
@@ -7,5 +7,5 @@ var text_ar_1 = document.getElementById("text_ar_1");
 
 //--------------- styling --------------------------------
 text_ar_1.style.position = "absolute";
-text_ar_1.style.height = ((s_h - 20)*2) + "px";
-text_ar_1.style.top = 20 + "px";
+/*text_ar_1.style.height = (s_h - 20) + "px";
+text_ar_1.style.top = 20 + "px";*/
